5ff246209f359cb66b041dc42af652fe80646764
dpvue <dpvue@users.noreply.github.com>
2020-02-12 15:11:56 +0200

    Create index-ro.php
    Romanian translation

---
+++ src/htdocs/admin/views/index-ro.php
@@ -0,0 +1,23 @@
+  
+<h1 class="display-4 logo text-center">aqi.eco</h1>
+
+<p class="lead text-justify">
+<span class="logo">aqi.eco</span> este un serviciu care permite afișarea măsurătorilor curente PM10 și PM2.5 realizate de dispozitivul dvs. <a href="https://luftdaten.info/">Luftdaten</a>.
+</p>
+
+<p class="text-center">
+<a class="btn btn-warning" href="https://smolna.aqi.eco/">Live demo</a>
+</p>
+
+<p class="text-justify">
+Înscrieți-vă, configurați senzorul și partajați pagina creată cu prietenii și familia pentru ca toată lumea să fie la curent
+cu informațiile despre calitatea aerului din cartierul tău.
+</p>
+
+<p class="text-justify">
+Pagina creată este atractivă atât de pe un browser desktop, cât și de pe un telefon mobil.
+</p>
+
+<p class="text-center">
+<img src="/admin/public/img/screenshot-en.png" width="100%"/>
+</p>
