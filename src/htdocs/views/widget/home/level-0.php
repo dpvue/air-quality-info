65e38d9877a1e8ec1aef88340a4030b1549dd748
dpvue <dpvue@users.noreply.github.com>
2020-02-14 11:14:28 +0200

    Create level-0.php
    English translation

---
+++ src/htdocs/views/widget/home/level-0.php
@@ -0,0 +1,82 @@
+<div class="row homewidget">
+  <div class="col-md-8 offset-md-2">
+
+    <div class="row">
+      <div class="col-md-6">
+
+        <ul class="list-group">
+          <li class="list-group-item text-center">
+            <h3 class="">Air quality</h3>
+          </li>
+
+          <li class="list-group-item text-center">
+            <i class="fa fa-smile-o fa-5x air-quality-0"></i>
+            <h3 class="air-quality-0">Okay</h3>
+          </li>
+        </ul>
+      </div>
+      
+      <?php include ("views/widget/home/weather.php"); ?>
+
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <ul class="list-group">
+          <li class="list-group-item text-center">
+            <span class="strip"><i class="icon-spacer green"></i>
+              <span class="strip-recommended">
+                <strong>Recommended</strong>
+                <br />
+                <span class="strip-recommended-description">walks outside</span>
+              </span>
+            </span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-6">
+        <ul class="list-group">
+          <li class="list-group-item text-center">
+            <span class="strip"><i class="icon-rower green"></i>
+              <span class="strip-recommended">
+                <strong>Recommended</strong>
+                <br />
+                <span class="strip-recommended-description">aktywność fizyczna na zewnątrz</span>
+              </span>
+            </span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-6">
+        <ul class="list-group">
+          <li class="list-group-item text-center">
+            <span class="strip"><i class="icon-tree green"></i>
+              <span class="strip-recommended">
+                <strong>Recommended</strong>
+                <br />
+                <span class="strip-recommended-description">przebywanie na świeżym powietrzu</span>
+              </span>
+            </span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-6">
+        <ul class="list-group">
+          <li class="list-group-item text-center">
+            <span class="strip"><i class="icon-elderly green"></i>
+              <span class="strip-recommended">
+                <strong>Recommended</strong>
+                <br />
+                <span class="strip-recommended-description">being outdoors (pregnancy, asthma, elderly)</span>
+              </span>
+            </span>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+</div>
